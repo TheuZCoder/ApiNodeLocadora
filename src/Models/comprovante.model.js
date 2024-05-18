522916c1145a29a9bb62c1c63213472850c7a5e9
TheuZCoder <matheusrdsilva1@gmail.com>
2024-05-18 11:43:34 -0300

    Feito teste feedback

--- src/Models/comprovante.model.js
+++ src/Models/comprovante.model.js
@@ -1,61 +1,92 @@
-const db = require('../Infraestrutura/database');
+const db = require("../Infraestrutura/database");
 
 const getComprovantes = async () => {
-    try {
-        const { rows } = await db.query('SELECT * FROM comprovante');
-        return rows;
-    } catch (error) {
-        console.log("Erro durante pesquisa de comprovantes: ", error)
-        res.status(500).json({ message: "Erro durante pesquisa de comprovantes" })
-    }
-}
+  try {
+    const { rows } = await db.query("SELECT * FROM comprovante");
+    return rows;
+  } catch (error) {
+    console.log("Erro durante pesquisa de comprovantes: ", error);
+    res.status(500).json({ message: "Erro durante pesquisa de comprovantes" });
+  }
+};
 
 const getComprovanteById = async (id) => {
-    try {
-        const { rows } = await db.query('SELECT * FROM comprovante WHERE id = $1', [id]);
-        return rows[0];
-    } catch (error) {
-        console.log("Erro durante pesquisa de comprovante por id: ", error)
-        res.status(500).json({ message: "Erro durante pesquisa de comprovante por id" })
-    }
-}
+  try {
+    const { rows } = await db.query(
+      "SELECT * FROM comprovante WHERE id_comprovante = $1",
+      [id]
+    );
+    return rows[0];
+  } catch (error) {
+    console.log("Erro durante pesquisa de comprovante por id: ", error);
+    res
+      .status(500)
+      .json({ message: "Erro durante pesquisa de comprovante por id" });
+  }
+};
 
 const createComprovante = async (comprovante) => {
-    try {
-        const { rows } = await db.query('INSERT INTO comprovante (data_pagamento, valor, id_locacao) VALUES ($1, $2, $3) RETURNING *', [comprovante.data_pagamento
-            , comprovante.valor, comprovante.id_locacao]);
-            return rows[0];
-    } catch (error) {
-        console.log("Erro durante criação de comprovante: ", error)
-        res.status(500).json({ message: "Erro durante criação de comprovante" })
-    }
-}
+  try {
+    const { rows } = await db.query(
+      "INSERT INTO comprovante (id_locacao, id_cliente, id_funcionario, data_pagamento, valor_pagamento, forma_pagamento, status_pagamento) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *",
+      [
+        comprovante.id_locacao,
+        comprovante.id_cliente,
+        comprovante.id_funcionario,
+        comprovante.data_pagamento,
+        comprovante.valor_pagamento,
+        comprovante.forma_pagamento,
+        comprovante.status_pagamento,
+      ]
+    );
+    return rows[0];
+  } catch (error) {
+    console.log("Erro durante criação de comprovante: ", error);
+    res.status(500).json({ message: "Erro durante criação de comprovante" });
+  }
+};
 
 const updateComprovante = async (id, comprovante) => {
-    try {
-        const { rows } = await db.query('UPDATE comprovante SET data_pagamento = $1, valor = $2, id_locacao = $3 WHERE id = $4 RETURNING *', [comprovante.data_pagamento
-            , comprovante.valor, comprovante.id_locacao, id]);
-        return rows[0];
-    } catch (error) {
-        console.log("Erro durante atualização de comprovante: ", error)
-        res.status(500).json({ message: "Erro durante atualização de comprovante" })
-    }
-}
+  try {
+    const { rows } = await db.query(
+      "UPDATE comprovante SET id_locacao = $1, id_cliente = $2, id_funcionario = $3, data_pagamento = $4, valor_pagamento = $5, forma_pagamento = $6, status_pagamento = $7 WHERE id_comprovante = $8 RETURNING *",
+      [
+        comprovante.id_locacao,
+        comprovante.id_cliente,
+        comprovante.id_funcionario,
+        comprovante.data_pagamento,
+        comprovante.valor_pagamento,
+        comprovante.forma_pagamento,
+        comprovante.status_pagamento,
+        comprovante.id_comprovante
+      ]
+    );
+    return rows[0];
+  } catch (error) {
+    console.log("Erro durante atualização de comprovante: ", error);
+    res
+      .status(500)
+      .json({ message: "Erro durante atualização de comprovante" });
+  }
+};
 
 const deleteComprovante = async (id) => {
-    try {
-        const { rows } = await db.query('DELETE FROM comprovante WHERE id = $1 RETURNING *', [id]);
-        return rows[0];
-    } catch (error) {
-        console.log("Erro durante exclusão de comprovante: ", error)
-        res.status(500).json({ message: "Erro durante exclusão de comprovante" })
-    }
-}
+  try {
+    const { rows } = await db.query(
+      "DELETE FROM comprovante WHERE id_comprovante = $1 RETURNING *",
+      [id]
+    );
+    return rows[0];
+  } catch (error) {
+    console.log("Erro durante exclusão de comprovante: ", error);
+    res.status(500).json({ message: "Erro durante exclusão de comprovante" });
+  }
+};
 
 module.exports = {
-    getComprovantes,
-    getComprovanteById,
-    createComprovante,
-    updateComprovante,
-    deleteComprovante
-}
+  getComprovantes,
+  getComprovanteById,
+  createComprovante,
+  updateComprovante,
+  deleteComprovante,
+};
--- src/Models/feedback.model.js
+++ src/Models/feedback.model.js
@@ -1,61 +1,86 @@
-const db = require('../Infraestrutura/database');
+const db = require("../Infraestrutura/database");
 
 const getFeedbacks = async () => {
-    try {
-        const { rows } = await db.query('SELECT * FROM feedback');
-        return rows;
-    } catch (error) {
-        console.log("Erro durante pesquisa de feedbacks: ", error)
-        res.status(500).json({ message: "Erro durante pesquisa de feedbacks" })
-    }
-}
+  try {
+    const { rows } = await db.query("SELECT * FROM feedback");
+    return rows;
+  } catch (error) {
+    console.log("Erro durante pesquisa de feedbacks: ", error);
+    res.status(500).json({ message: "Erro durante pesquisa de feedbacks" });
+  }
+};
 
 const getFeedbackById = async (id) => {
-    try {
-        const { rows } = await db.query('SELECT * FROM feedback WHERE id = $1', [id]);
-        return rows[0];
-    } catch (error) {
-        console.log("Erro durante pesquisa de feedback por id: ", error)
-        res.status(500).json({ message: "Erro durante pesquisa de feedback por id" })
-    }
-}
+  try {
+    const { rows } = await db.query(
+      "SELECT * FROM feedback WHERE id_feedback = $1",
+      [id]
+    );
+    return rows[0];
+  } catch (error) {
+    console.log("Erro durante pesquisa de feedback por id: ", error);
+    res
+      .status(500)
+      .json({ message: "Erro durante pesquisa de feedback por id" });
+  }
+};
 
 const createFeedback = async (feedback) => {
-    try {
-        const { rows } = await db.query('INSERT INTO feedback (nota, comentario, id_cliente) VALUES ($1, $2, $3) RETURNING *', [feedback.nota
-            , feedback.comentario, feedback.id_cliente]);
-        return rows[0];
-    } catch (error) {
-        console.log("Erro durante criação de feedback: ", error)
-        res.status(500).json({ message: "Erro durante criação de feedback" })
-    }
-}
+  try {
+    const { rows } = await db.query(
+      "INSERT INTO feedback (id_locacao, id_cliente, data_feedback, avaliacao, comentario) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [
+        feedback.id_locacao,
+        feedback.id_cliente,
+        feedback.data_feedback,
+        feedback.avaliacao,
+        feedback.comentario,
+      ]
+    );
+    return rows[0];
+  } catch (error) {
+    console.log("Erro durante criação de feedback: ", error);
+    res.status(500).json({ message: "Erro durante criação de feedback" });
+  }
+};
 
 const updateFeedback = async (id, feedback) => {
-    try {
-        const { rows } = await db.query('UPDATE feedback SET nota = $1, comentario = $2, id_cliente = $3 WHERE id = $4 RETURNING *', [feedback.nota
-            , feedback.comentario, feedback.id_cliente, id]);
-        return rows[0];
-    } catch (error) {
-        console.log("Erro durante atualização de feedback: ", error)
-        res.status(500).json({ message: "Erro durante atualização de feedback" })
-    }
-}
+  try {
+    const { rows } = await db.query(
+      "UPDATE feedback SET id_locacao = $1, id_cliente = $2, data_feedback = $3, avaliacao = $4, comentario = $5  WHERE id_feedback = $6 RETURNING *",
+      [
+        feedback.id_locacao,
+        feedback.id_cliente,
+        feedback.data_feedback,
+        feedback.avaliacao,
+        feedback.comentario,
+        feedback.id_feedback
+      ]
+    );
+    return rows[0];
+  } catch (error) {
+    console.log("Erro durante atualização de feedback: ", error);
+    res.status(500).json({ message: "Erro durante atualização de feedback" });
+  }
+};
 
 const deleteFeedback = async (id) => {
-    try {
-        const { rows } = await db.query('DELETE FROM feedback WHERE id = $1 RETURNING *', [id]);
-        return rows[0];
-    } catch (error) {
-        console.log("Erro durante exclusão de feedback: ", error)
-        res.status(500).json({ message: "Erro durante exclusão de feedback" })
-    }
-}
+  try {
+    const { rows } = await db.query(
+      "DELETE FROM feedback WHERE id_feedback = $1 RETURNING *",
+      [id]
+    );
+    return rows[0];
+  } catch (error) {
+    console.log("Erro durante exclusão de feedback: ", error);
+    res.status(500).json({ message: "Erro durante exclusão de feedback" });
+  }
+};
 
 module.exports = {
-    getFeedbacks,
-    getFeedbackById,
-    createFeedback,
-    updateFeedback,
-    deleteFeedback
-}
+  getFeedbacks,
+  getFeedbackById,
+  createFeedback,
+  updateFeedback,
+  deleteFeedback,
+};
